bd0314d90039e52b007466ce43bcfda1e2b97ab6
Akshay <jainakshay058@gmail.com>
2023-03-25 14:56:39 +0530

    add basic thunk

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,22 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { add } from "../store/cartSlice";
 import { useDispatch } from "react-redux";
+import { fetchProducts } from "../store/productSlice";
+import { useSelector } from "react-redux";
+import { STATUSES } from "../store/productSlice";
 export default function Products() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
+  const { data: products, status } = useSelector((state) => state.product);
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      console.log(data);
-      setProducts(data);
-    };
-    fetchProducts();
+    dispatch(fetchProducts());
+
+    // const fetchProducts = async () => {
+    //   const res = await fetch("https://fakestoreapi.com/products");
+    //   const data = await res.json();
+    //   console.log(data);
+    //   setProducts(data);
+    // };
+    // fetchProducts();
   }, []);
 
   const handleAdd = (product) => {
     dispatch(add(product));
   };
+
+  // check if loading state
+  if (status === STATUSES.LOADING) {
+    return <h2>Loading...</h2>;
+  }
+  if (status === STATUSES.ERROR) {
+    return <h2>Something went Wrong!</h2>;
+  }
+
   return (
     <div className="productsWrapper">
       {products.map((product) => (
